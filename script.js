6b97ba2437b1c91deb4414c319adc011de933124
sameer-codes <140482587+sameer-codes@users.noreply.github.com>
2023-07-26 11:32:04 +0000

    feature: translating text into banana language
    1. getting the text translated in to banana language through API call
    2.showing output in text area with java script

--- script.js
+++ script.js
@@ -33,20 +33,23 @@
 var primaryButton = document.querySelector("#primary-button");
 var txtInput = document.querySelector("#txt-area");
 var outputDiv = document.querySelector("#output");
-//text area refernce in javascript
+//text area refernce in javascript for dynamic output
 
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
+// url for api call from server
 
 function getTranslatedURL(text){
   return serverURL + "?" + "text=" + text
 }
+// getting the translation from api
 
 function errorHandler(error)
 {
   console.log("error occurred"+ error);
   alert("something happend to the server , try after some time")
 }
+// error handleing in case the server is not working
 
 function clickEventHandler() {
     var inputText= txtInput.value;
@@ -56,11 +59,11 @@ function clickEventHandler() {
     .then(response => response.json())
     .then(json => {
       var translatedText=json.contents.translated;
-      outputDiv.innerText = translatedText;
+      outputDiv.innerText = translatedText;                // showing output in the text area 
     }
          )
   
-   .catch(errorHandler)
+   .catch(errorHandler)                                    // error handling call
 };
 
-primaryButton.addEventListener("click", clickEventHandler);
+primaryButton.addEventListener("click", clickEventHandler); // listing to when the button is clicked
